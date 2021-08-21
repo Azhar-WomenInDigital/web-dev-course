93474ab8996e19559ac14c327d828219ec23ebba
Azhar-WomenInDigital <75774469+Azhar-WomenInDigital@users.noreply.github.com>
2021-08-21 12:55:05 +0600

    initial-commit

---
+++ Bootstrap-Projects/assets/js/main.js
@@ -0,0 +1,13 @@
+var typed = new Typed('#typed', {
+    strings: [
+        'Search  Course : Web Design', 
+        'Search  Course : Web Development', 
+        'Search  Course : Graphic Design', 
+        'Search  Course : Digital  Marketing', 
+    ],
+    typeSpeed: 60,
+    backSpeed: 60,
+    attr: 'placeholder',
+    bindInputFocusEvents: true,
+    loop: true
+});
